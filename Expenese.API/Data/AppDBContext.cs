aa8927faa11babe67c4fa3fd25e2d273cd1ae7bb
Charan <charanbabu9686@gmail.com>
2025-11-05 23:36:16 +0530

    dbcontext and Ef code commits

--- Expenese.API/Data/AppDBContext.cs
+++ Expenese.API/Data/AppDBContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Expenese.API.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Expenese.API.Data
 {
     public class AppDBContext : DbContext
     {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
--- Expenese.API/Program.cs
+++ Expenese.API/Program.cs
@@ -1,8 +1,13 @@
+using Expenese.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
+var conn = builder.Configuration.GetConnectionString("Default");
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDBContext>(opt => opt.UseSqlServer(conn));
 builder.Services.AddControllers();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
